c395ff3b3ec1f06af5dd143ecc784c816e861551
Dominik <dforstmaier@gmail.com>
2023-08-29 19:04:31 +0200

    Git Commit

---
+++ GuckkastenGame/Source/GuckkastenGame/GuckkastenGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GuckkastenGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUCKKASTENGAME_API AGuckkastenGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
